d6a3b59abaa5a530aafac2a5554da4b59250b2ec
Yoannes Figueiras <yoannesfigueiras@Yoanness-MacBook-Pro.local>
2022-10-11 22:42:16 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  username: string = '';
+  
+  clickme(word1:string,word2:string) {
+    let fruitsColor: string[] = ['Apple', 'red', 'Orange', 'orange', 'Banana' , 'yellow'];
+    console.log('input1: ',word1);
+    console.log('input2: ',word2);
+
+   
+    if(fruitsColor.includes(word1) && (fruitsColor[fruitsColor.indexOf(word1) + 1] === word2)){
+      console.log('Good job!!!:');
+    }else{console.log('Wrong word:')}
+    
+  }
+
+
 }
